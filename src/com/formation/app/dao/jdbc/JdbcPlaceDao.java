b6f1e3713711d2e5515d1f345d239d4fc113000b
CorentinPineault <p-corentin@outlook.fr>
2021-06-04 13:46:04 +0200

    Ajout implémentation interfaces

--- src/com/formation/app/dao/jdbc/JdbcPlaceDao.java
+++ src/com/formation/app/dao/jdbc/JdbcPlaceDao.java
@@ -1,4 +1,33 @@
 package com.formation.app.dao.jdbc;
 
-public class JdbcPlaceDao extends JdbcDao{
+import com.formation.app.Place;
+import com.formation.app.dao.PlaceDao;
+
+import java.util.List;
+
+public class JdbcPlaceDao extends JdbcDao implements PlaceDao{
+    @Override
+    public Long createPlace(Place place) {
+        return null;
+    }
+
+    @Override
+    public Place findPlaceById(Long id) {
+        return null;
+    }
+
+    @Override
+    public boolean updatePlace(Place place) {
+        return false;
+    }
+
+    @Override
+    public boolean removePlace(Place place) {
+        return false;
+    }
+
+    @Override
+    public List<Place> findAllPlace() {
+        return null;
+    }
 }
--- src/com/formation/app/dao/jdbc/JdbcTripDao.java
+++ src/com/formation/app/dao/jdbc/JdbcTripDao.java
@@ -1,4 +1,33 @@
 package com.formation.app.dao.jdbc;
 
-public class JdbcTripDao extends JdbcDao{
+import com.formation.app.Trip;
+import com.formation.app.dao.TripDao;
+
+import java.util.List;
+
+public class JdbcTripDao extends JdbcDao implements TripDao {
+    @Override
+    public Long createTrip(Trip trip) {
+        return null;
+    }
+
+    @Override
+    public Trip findTripById(Long id) {
+        return null;
+    }
+
+    @Override
+    public boolean updateTrip(Trip trip) {
+        return false;
+    }
+
+    @Override
+    public boolean removeTrip(Trip trip) {
+        return false;
+    }
+
+    @Override
+    public List<Trip> findAllTrip() {
+        return null;
+    }
 }
